668b085924c4a1802c173b34cc2bde750ecf98ed
Nolan Kovacik <noltron000@gmail.com>
2021-01-27 02:08:12 -0500

    [add] working solution

--- count-clouds.js
+++ count-clouds.js
@@ -1,6 +1,126 @@
+class Stack {
+	constructor(...data) {
+		this.data = data
+	}
+
+	// get the "height" of the stack, or number of nodes.
+	get size () {
+		return this.data.length
+	}
+
+	// the stack has nodes if its size is greater than zero.
+	get hasNodes () {
+		return this.size !== 0
+	}
+
+	// add an node to the "top" of the stack.
+	push (node) {
+		this.data.push(node)
+	}
+
+	// remove a node from the "top" of the stack,
+	// and then return it.
+	pop () {
+
+		// get the result, which might be undefined.
+		const result = this.data.pop()
+
+		// filter out the possibility of returning undefined.
+		if (result !== undefined) {
+			return result
+		} else {
+			throw new Error('Cannot pop an empty stack!')
+		}
+	}
+
+	// return the node at the "top" of the stack,
+	// but do not remove it.
+	peek () {
+
+		// get the result, which might be undefined.
+		const result = this.data.slice(-1)[0]
+
+		// filter out the possibility of returning undefined.
+		if (result !== undefined) {
+			return result
+		} else {
+			throw new Error('Cannot peek into an empty stack!')
+		}
+	}
+}
+
+/**********************************************************/
+
 // skyMap is a 2D array where indices are X, Y coordinates.
 const countClouds = (skyMap) => {
-	return bruteForce(skyMap)
+	return searchSolution(skyMap)
+}
+
+const searchSolution = (skyMap) => {
+	// initialize tracker variables
+	const length = skyMap.length
+	const width = skyMap[0]?.length
+	const visited = new Set()
+	const clouds = new Set()
+
+	// implement depth-first search
+	for (let xPos = 0; xPos < width; xPos += 1) {
+		for (let yPos = 0; yPos < length; yPos += 1) {
+			const coords = JSON.stringify([yPos, xPos])
+
+			// create a new coordoinate cloud
+			const cloud = new Set()
+
+			// implement depth-first search
+			const stack = new Stack(coords)
+			while (stack.hasNodes) {
+
+				// determine coords, positions, and value.
+				const coords01 = stack.peek()
+				const [yPos01, xPos01] = JSON.parse(coords01)
+				const value01 = skyMap[yPos01]?.[xPos01]
+
+				// short-circuit if theres a void- or zero-value,
+				// or if these coords have been visited before.
+				if (value01 === undefined || visited.has(coords01)) {
+					stack.pop()
+					continue
+				}
+				else if (value01 === '0') {
+					visited.add(coords01)
+					stack.pop()
+					continue
+				}
+
+				// determine all possible neighbors
+				const northCoords = JSON.stringify([yPos01 - 1, xPos01])
+				const southCoords = JSON.stringify([yPos01 + 1, xPos01])
+				const westCoords =  JSON.stringify([yPos01, xPos01 - 1])
+				const eastCoords =  JSON.stringify([yPos01, xPos01 + 1])
+				const neighbors = new Set([
+					northCoords,
+					southCoords,
+					westCoords,
+					eastCoords,
+				])
+
+				// add all neighbors to stack
+				for (const coords02 of neighbors) {
+					stack.push(coords02)
+				}
+
+				// add coordinates as visited and into the cloud
+				cloud.add(coords01)
+				visited.add(coords01)
+			}
+
+			// add cloud to clouds, if its not empty
+			if (cloud.size > 0) {
+				clouds.add(cloud)
+			}
+		}
+	}
+	return clouds.size
 }
 
 const bruteForce = (skyMap) => {
@@ -13,6 +133,7 @@ const bruteForce = (skyMap) => {
 			// ignore this coordinates if its value is zero.
 			if (skyMap[yPos]?.[xPos] === '0') {
 				continue
+			}
 
 			// in these coordinates arrays,
 			// notice its [y, x] not [x, y].
